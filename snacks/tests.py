22097ffa66959c6fd580a1ea5abda06b33bb4c8a
adhammhaydat <adhammohidat123@gmail.com>
2021-11-15 17:55:34 +0200

    solve lab 27

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.http import response
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SnacksTests(TestCase):
+    def test_list_page_status_code(self):
+        url = reverse("list_view")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse("list_view")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "base.html")
+    
+    
+              
